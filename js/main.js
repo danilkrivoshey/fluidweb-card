5eb593bb7163b9d68a16e2c05bd9c4978c08e55e
Danil Krivoshey <danil@fluidweb.io>
2020-09-01 18:15:41 +0300

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 window.addEventListener('resize', main);
 window.addEventListener('load', main);
 
+var granimInstance = new Granim({
+    element: '#granim-canvas',
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#f1e5e8', '#e7edff'],
+                ['#cde8fa', '#f2f5e3'],
+                ['#edebfc', '#f1e5e8'],
+            ],
+            transitionSpeed: 2000
+        }
+    }
+});
+
 function main() {
     var canvasWrapper = document.querySelector('.canvas-wrapper');
 
     document.addEventListener('touchmove', function (e) {
         e.preventDefault();
-    });
-
-    var granimInstance = new Granim({
-        element: '#granim-canvas',
-        isPausedWhenNotInView: true,
-        states: {
-            "default-state": {
-                gradients: [
-                    ['#f1e5e8', '#e7edff'],
-                    ['#cde8fa', '#f2f5e3'],
-                    ['#edebfc', '#f1e5e8'],
-                ],
-                transitionSpeed: 2000
-            }
-        }
     });
 
     var shareButton = document.getElementById('share-btn');
@@ -30,6 +30,7 @@ function main() {
         shareButton.addEventListener('click', function () {
             navigator.share({
                 title: 'Follow the link to contact information \n Maryna Kisil',
+                text: 'Follow the link to contact information \n Maryna Kisil',
                 url: 'https://danilkrivoshey.github.io/fluidweb-card/'
             }).then(() => {
                 console.log('Thanks for sharing!');
