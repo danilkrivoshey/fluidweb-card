c90c0a30d3b4d1f433a4904889362ded55542ba7
Danil Krivoshey <danil@fluidweb.io>
2020-09-01 18:10:45 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -23,28 +23,28 @@ function main() {
         }
     });
 
-    if (!window.matchMedia('(max-width: 1024px').matches) {
-        var scene = document.getElementById("parallax");
-        var parallax = new Parallax(scene, {
-            relativeInput: true,
-        });
+    var shareButton = document.getElementById('share-btn');
 
+    if (navigator.share) {
+        shareButton.classList.add("active");
+        shareButton.addEventListener('click', function () {
+            navigator.share({
+                title: 'Follow the link to contact information \n Maryna Kisil',
+                url: 'https://danilkrivoshey.github.io/fluidweb-card/'
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            }).catch(console.error);
+        });
+    }
+
+    var scene = document.getElementById("parallax");
+    var parallax = new Parallax(scene, {
+        relativeInput: true,
+    });
+
+    if (!window.matchMedia('(max-width: 1024px').matches) {
         canvasWrapper.classList.add('remove');
     } else {
         canvasWrapper.classList.remove('remove');
-
-        var shareButton = document.getElementById('share-btn');
-
-        if (navigator.share || navigator.canShare) {
-            shareButton.classList.add("active");
-            shareButton.addEventListener('click', function () {
-                navigator.share({
-                    title: 'WebShare API Demo',
-                    url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
-                }).then(() => {
-                    console.log('Thanks for sharing!');
-                }).catch(console.error);
-            });
-        }
     }
 }
